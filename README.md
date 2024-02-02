1d888bae7beb3214cc853414b3cdc1fbb286c8c9
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-02 19:57:06 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Homebrew Workspace
+
+This is a template workspace with homebrew. It is a starting point for creating a new workspace.
+
---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "packages" {
+  type = list(string)
+  description = "Packages to install"
+  default = []
+}
+
+variable "hostname" {
+  type = string
+  description = "Hostname of the container"
+  default = null
+} 
+
+variable "image" {
+  type = string
+  description = "Image to use, required if context is not set"
+  default = "ghcr.io/pocketenv-io/homebrew:main"
+}
+
+variable "workspace_name" {
+  type = string
+  description = "Name of the workspace"
+  default = "brew-workspace"
+}
+
+variable "user" {
+  type = string
+  description = "User to create"
+  default = "coder"
+}
+
+variable "context" {
+  type = string
+  description = "Context to use"
+  default = null
+}
+
+variable "volumes" {
+  type = map(string)
+  description = "Volumes to mount"
+  default = {
+    brew-workspace = "/home/coder"
+  }
+}
